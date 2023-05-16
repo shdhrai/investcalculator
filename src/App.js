49facb7c18cd5ba71261787fdf133287f08a8293
shdhrai <shdhrai@gmail.com>
2023-05-16 19:10:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [amount ="", setAmount] = useState();
+  const [dividend, setDividend] = useState();
+  const [income, setIncome] = useState();
+
+  const clickHandler = () => {
+    const a = income * (100 / dividend);
+    setAmount(Math.trunc(a));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form >
+        <div className='form-div'>
+          <p>Dividend Yield </p>
+
+          <input value={dividend} placeholder='Dividend Yield' type='number'
+            onChange={(e) => { setDividend(e.target.value) }}>
+          </input>
+
+          <p>Target Income</p>
+
+          <input value={income} placeholder='Target Income' type='number'
+            onChange={(e) => { setIncome(e.target.value) }}>
+          </input>
+
+          <br></br>
+
+          <button type='button' onClick={clickHandler} >Submit</button>
+
+          <p>You will have to invest {amount}</p>
+        </div>
+      </form>
     </div>
   );
 }
